53c470742c2584a263a49bad090010f7ed0bd97f
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-15 09:03:04 -0700

    More Changes

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+'use strict';
+
 const STORE = [
-  {name: "apples", checked: false},
-  {name: "oranges", checked: false},
-  {name: "milk", checked: true},
-  {name: "bread", checked: false}
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: true},
+  {name: 'bread', checked: false}
 ];
 
+function generateItemElement(item, itemIndex, template) {
+  return `
+  <li class="js-item-index-element" data-item-index="${itemIndex}">
+    <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+}
+
+function generateShoppingItemsString(shoppingList) {
+  console.log('Generating shopping list element');
+
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  
+  return items.join('');
+}
+
 function renderShoppingList() {
-  // this function will be responsible for rendering the shopping list in
-  // the DOM
+  // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 
